266c14158520151de8b4092e409d12bcd36db40e
Nam Le <hai.le@student.tut.fi>
2019-08-11 21:57:03 +0300

    merge

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get 'mainpage/index', as: :index
-  # get 'mainpage/index/:day_shift', to: "mainpage#index", as: :mainpage_index_p
-  root 'mainpage#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
