415e097f58ae617435f70b87c42c2f096172b2ba
Nam Le <nam.le@adalia.fi>
2019-08-13 11:02:28 +0300

    gem requirements

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,6 @@
 require_relative 'boot'
 
-# require 'rails/all'
-require "active_storage/engine"
-# require 'action_controller/railtie'
-require 'action_mailer/railtie'
-# require 'active_model/railtie'
-require 'sprockets/railtie'
-# require 'rails/test_unit/railtie'
-require 'action_cable/engine'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
