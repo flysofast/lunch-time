e50364224d6943dfe251fed2af7b0e1df7408e11
namle <namle@localhost.localdomain>
2019-08-22 12:23:23 +0300

    Timezone set to Helsinki

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module CampusMenu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.time_zone = 'Helsinki'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
